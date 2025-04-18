50679ace4c52a70e6801f5cf22c18017a380e7a5
Ajinkya Kumawat <lovefoolscoffeeorders@gmail.com>
2025-04-18 13:06:28 +0530

    issue fixes

--- collection/PaymentController.js
+++ collection/PaymentController.js
@@ -44,6 +44,7 @@ const InitiatePayment = async (req, res) => {
 };
 
 const HandlePaymentresponse = async (req, res) => {
+  console.log("body",req.body)
   await new Promise((resolve) => upload.any()(req, res, resolve)); // Parse FormData
 
   const orderId = req.body.order_id || req.body.orderId;

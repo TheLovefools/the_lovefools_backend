2d5b9150699cc00077cf0d366dcfdacb214062a8
JamesSwarge <james.swarge@gmail.com>
2025-05-01 18:58:59 +0530

    Changes for whatsapp OTP.

--- collection/WhatsappOtpController.js
+++ collection/WhatsappOtpController.js
@@ -17,7 +17,7 @@ const WhatsappOtp = async (req, res) => {
           parameters: [
             {
               type: "text",
-              text: Number(otp)
+              text: otp
             }
           ]
         }
